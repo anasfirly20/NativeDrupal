5d97eef87b01d8c41363cdf7b981582512912631
Anas Firly <anasfirly20@gmail.com>
2023-07-11 08:55:01 +0300

    fixing font issues

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: [
+    './node_modules/react-native-vector-icons/FONTS/AntDesign.ttf',
+    './node_modules/react-native-vector-icons/FONTS/FontAwesome.ttf',
+    './node_modules/react-native-vector-icons/FONTS/MaterialCommunityIcons.ttf',
+    './node_modules/react-native-vector-icons/FONTS/MaterialIcons.ttf',
+  ],
+};
